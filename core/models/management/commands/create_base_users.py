fd9b60654d0e0bacaa0fd1f0479cdd8859843042
Anton Knyazev <knyazevmac@icloud.com>
2024-11-02 21:57:39 +0300

    Обновлена логика комманды python manage.py create_base_users теперь для каждого пользователя создаются уникальные идентификационные номера

--- core/models/management/commands/create_base_users.py
+++ core/models/management/commands/create_base_users.py
@@ -4,6 +4,7 @@ from models.config import engine
 from django.contrib.auth.hashers import make_password
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+import random
 
 
 """
@@ -39,6 +40,31 @@ class Command(BaseCommand):
             with Session(engine) as session:
 
                 # NOTE - Создаем пользователя с ролью 'пользователь'
+                # Создаем уникальный идентификатор для пользователя
+                user_id_number: int
+                while True:
+                    with Session(engine) as session:
+                        # Генерируем случайный идентификатор
+                        user_id_number = random.randrange(0, 1000000, 1)
+                        
+                        # Создаем sql запрос
+                        users_sql_row = select(User)
+
+                        # Получаем всех пользователей
+                        users = session.scalars(users_sql_row).all()
+
+                        check_id_number: bool = False
+
+                        # Проверяем, есть ли такой идентификатор у другого пользователя
+                        for i in range(len(users)):
+                            if users[i].id_number == user_id_number:
+                                check_id_number = True
+                                break
+
+                        if not check_id_number:
+                            break
+
+
                 # Формируем пароль для пользователя
                 user_password = make_password("user")
                 
@@ -49,11 +75,36 @@ class Command(BaseCommand):
                 user_role_id = session.scalar(user_role).id
 
                 # Создаем нового пользователя с ролью 'пользователь' и паролем 'user'
-                new_user = User(username="user", password=user_password,
+                new_user = User(id_number=user_id_number,
+                                username="user", password=user_password,
                                 role_id=user_role_id, gender="мужчина")
                 
 
                 # NOTE - Создаем пользователя с ролью 'организатор'
+                # Создаем уникальный идентификатор для пользователя
+                organizer_id_number: int
+                while True:
+                    with Session(engine) as session:
+                        # Генерируем случайный идентификатор
+                        organizer_id_number = random.randrange(0, 1000000, 1)
+                        
+                        # Создаем sql запрос
+                        users_sql_row = select(User)
+
+                        # Получаем всех пользователей
+                        users = session.scalars(users_sql_row).all()
+
+                        check_id_number: bool = False
+
+                        # Проверяем, есть ли такой идентификатор у другого пользователя
+                        for i in range(len(users)):
+                            if users[i].id_number == organizer_id_number:
+                                check_id_number = True
+                                break
+
+                        if not check_id_number:
+                            break
+
                 # Формируем пароль для организатора
                 organizer_password = make_password("organizer")
                 
@@ -64,11 +115,36 @@ class Command(BaseCommand):
                 organizer_role_id = session.scalar(organizer_role).id
 
                 # Создаем нового пользователя с ролью 'организатор' и паролем 'organizer'
-                new_organizer = User(username="organizer", password=organizer_password,
-                                role_id=organizer_role_id, gender="мужчина")
+                new_organizer = User(id_number=organizer_id_number,
+                                     username="organizer", password=organizer_password,
+                                     role_id=organizer_role_id, gender="мужчина")
                 
                 
                 # NOTE - Создаем пользователя с ролью 'модератор'
+                # Создаем уникальный идентификатор для пользователя
+                moderator_id_number: int
+                while True:
+                    with Session(engine) as session:
+                        # Генерируем случайный идентификатор
+                        moderator_id_number = random.randrange(0, 1000000, 1)
+                        
+                        # Создаем sql запрос
+                        users_sql_row = select(User)
+
+                        # Получаем всех пользователей
+                        users = session.scalars(users_sql_row).all()
+
+                        check_id_number: bool = False
+
+                        # Проверяем, есть ли такой идентификатор у другого пользователя
+                        for i in range(len(users)):
+                            if users[i].id_number == moderator_id_number:
+                                check_id_number = True
+                                break
+
+                        if not check_id_number:
+                            break
+
                 # Формируем пароль для модератора
                 moderator_password = make_password("moderator")
                 
@@ -79,11 +155,36 @@ class Command(BaseCommand):
                 moderator_role_id = session.scalar(moderator_role).id
 
                 # Создаем нового пользователя с ролью 'модератор' и паролем 'moderator'
-                new_moderator = User(username="moderator", password=moderator_password,
-                                role_id=moderator_role_id, gender="мужчина")
+                new_moderator = User(id_number=moderator_id_number,
+                                     username="moderator", password=moderator_password,
+                                     role_id=moderator_role_id, gender="мужчина")
 
 
                 # NOTE - Создаем пользователя с ролью 'жюри'
+                # Создаем уникальный идентификатор для пользователя
+                jury_id_number: int
+                while True:
+                    with Session(engine) as session:
+                        # Генерируем случайный идентификатор
+                        jury_id_number = random.randrange(0, 1000000, 1)
+                        
+                        # Создаем sql запрос
+                        users_sql_row = select(User)
+
+                        # Получаем всех пользователей
+                        users = session.scalars(users_sql_row).all()
+
+                        check_id_number: bool = False
+
+                        # Проверяем, есть ли такой идентификатор у другого пользователя
+                        for i in range(len(users)):
+                            if users[i].id_number == jury_id_number:
+                                check_id_number = True
+                                break
+
+                        if not check_id_number:
+                            break
+                        
                 # Формируем пароль для пользователя
                 jury_password = make_password("jury")
                 
@@ -94,7 +195,8 @@ class Command(BaseCommand):
                 jury_role_id = session.scalar(jury_role).id
 
                 # Создаем нового пользователя с ролью 'жюри' и паролем 'jury'
-                new_jury = User(username="jury", password=jury_password,
+                new_jury = User(id_number=jury_id_number,
+                                username="jury", password=jury_password,
                                 role_id=jury_role_id, gender="мужчина")
                                 
 
